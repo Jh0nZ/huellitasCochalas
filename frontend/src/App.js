2922a099bfb221f1161a758d42ed0ab1a8828822
Jh0nZ <jhondeycraft776@gmail.com>
2024-09-13 23:28:27 -0400

    ejemplo libreria

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
-import './App.css';
-import NavBar from './Components/NavBar';
-
+// App.js
+import React from 'react';
+import { Container, Typography, Button, Box } from '@mui/material';
 
 function App() {
   return (
-   <div className="App">
-  <NavBar/>
-  
-   </div>
+    <Container>
+      <Box sx={{ textAlign: 'center', mt: 4 }}>
+        <Typography variant="h2" component="h1" gutterBottom>
+          Hola Mundo
+        </Typography>
+        <Typography variant="body1">
+          Bienvenido a tu aplicación React con Material-UI.
+        </Typography>
+        <Button variant="contained" color="primary">
+          ¡Haz clic aquí!
+        </Button>
+      </Box>
+    </Container>
   );
 }
 

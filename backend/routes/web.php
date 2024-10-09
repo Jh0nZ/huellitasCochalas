bc6868cb53b5e4387da347719f10ff61383cb413
Jh0nZ <43425433+Jh0nZ@users.noreply.github.com>
2024-10-09 08:13:13 -0400

    build

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/{any}', function () {
+    return view('index');
+})->where('any', '^(?!api).*$');
+
+
 Route::get('/', function () {
     return view('welcome');
 });

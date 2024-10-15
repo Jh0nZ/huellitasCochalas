1966d0db42e36d25229c2445ed3b64a316ca6d7f
Jh0nZ <43425433+Jh0nZ@users.noreply.github.com>
2024-10-15 19:13:11 -0400

    quitar el front static del backend

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{any}', function () {
-    return view('index');
-})->where('any', '^(?!api).*$');
+// Route::get('/{any}', function () {
+//     return view('index');
+// })->where('any', '^(?!api).*$');
 
 
 Route::get('/', function () {

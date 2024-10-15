02308a894cd4435666a46358ed93e3fa5f957472
Jh0nZ <43425433+Jh0nZ@users.noreply.github.com>
2024-10-15 00:39:25 -0400

    agregar rutas

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SizeController;
+use App\Http\Controllers\AdoptionRequestController;
+use App\Http\Controllers\AdoptionRequestImageController;
+use App\Http\Controllers\BreedController;
+use App\Http\Controllers\ImageController;
+use App\Http\Controllers\PetImageController;
+use App\Http\Controllers\PetController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -15,3 +22,11 @@ Route::post('/example', function (Request $request) {
     $data = $request->all();
     return response()->json(['message' => 'This is a POST request', 'data' => $data]);
 });
+
+Route::apiResource('adoption-requests', AdoptionRequestController::class);
+Route::apiResource('adoption-request-images', AdoptionRequestImageController::class);
+Route::apiResource('breeds', BreedController::class);
+Route::apiResource('images', ImageController::class);
+Route::apiResource('pets', PetController::class);
+Route::apiResource('pet-images', PetImageController::class);
+Route::apiResource('sizes', SizeController::class);

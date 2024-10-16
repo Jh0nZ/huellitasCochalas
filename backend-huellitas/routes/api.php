64513945ceb1b8641e82ef392e099673ff21b1b1
Jh0nZ <43425433+Jh0nZ@users.noreply.github.com>
2024-10-15 20:53:57 -0400

    agregar sizes

--- backend-huellitas/routes/api.php
+++ backend-huellitas/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BreedController;
+use App\Http\Controllers\SizeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('breeds', BreedController::class);
+Route::apiResource('sizes', SizeController::class);
+

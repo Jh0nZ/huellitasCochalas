d2d9916a8d3b140318947f09903ca84158f5f45c
Jh0nZ <43425433+Jh0nZ@users.noreply.github.com>
2024-10-15 20:57:24 -0400

    agregar pets

--- backend-huellitas/routes/api.php
+++ backend-huellitas/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\BreedController;
 use App\Http\Controllers\ImageController;
+use App\Http\Controllers\PetController;
 use App\Http\Controllers\SizeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -24,4 +25,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::apiResource('breeds', BreedController::class);
 Route::apiResource('sizes', SizeController::class);
 Route::apiResource('images', ImageController::class);
+Route::apiResource('pets', PetController::class);
 

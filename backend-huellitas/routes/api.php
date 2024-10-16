ee9e5b93e622b19d0af41319e7796b8513523f72
Jh0nZ <43425433+Jh0nZ@users.noreply.github.com>
2024-10-15 20:58:47 -0400

    agregar pet_images

--- backend-huellitas/routes/api.php
+++ backend-huellitas/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\BreedController;
 use App\Http\Controllers\ImageController;
 use App\Http\Controllers\PetController;
+use App\Http\Controllers\PetImageController;
 use App\Http\Controllers\SizeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -26,4 +27,5 @@ Route::apiResource('breeds', BreedController::class);
 Route::apiResource('sizes', SizeController::class);
 Route::apiResource('images', ImageController::class);
 Route::apiResource('pets', PetController::class);
+Route::apiResource('pet-images', PetImageController::class);
 

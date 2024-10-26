cef59bbc9ce9e0c296b0ab79f26cf7cc072d5af4
Dayana200505 <128557082+Dayana200505@users.noreply.github.com>
2024-10-25 21:58:12 -0400

    Parte del menu del filtro

--- frontend/src/pages/Pets.jsx
+++ frontend/src/pages/Pets.jsx
@@ -1,10 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import {
-  Container, Typography, Box, Button, Menu, useTheme,
-} from '@mui/material';
+  Container,
+  Typography,
+  Box,
+  Button,
+  Menu,
+  FormControlLabel,
+  Checkbox,
+  useTheme,
+  Radio,
+  RadioGroup,
+} from "@mui/material";
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
 import { PetCard } from '../Components';
 import { useGetAllPetsQuery } from '../features/api/apiSlice'; // Importa el hook
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
 
 const Pets = () => {
   const { data, error, isError, isFetching, isSuccess } = useGetAllPetsQuery();
@@ -24,7 +35,12 @@ const Pets = () => {
   const handleFilterChange = (e) => {
     setFilter(e.target.value);
   };
-
+  const ageOptions = [
+    '1-3',
+    '4-6',
+    '7-9',
+    '10-12'
+  ];
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
@@ -71,12 +87,74 @@ const Pets = () => {
           </Button>
 
           <Menu
-            anchorEl={anchorEl}
-            open={Boolean(anchorEl)}
-            onClose={handleClose}
-          >
-            {/* Tu menú de filtros aquí */}
-          </Menu>
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "center",
+              }}
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "center",
+              }}
+            >
+              <Typography sx={{ px: 2, py: 1, fontWeight: "bold" , alignItems:"center"}}>
+                FILTRAR POR
+              </Typography>
+              <Typography sx={{ px: 2, py: 1, fontWeight: "bold" }}>
+                TAMAÑO
+              </Typography>
+              <FormControlLabel 
+                control={<Checkbox defaultChecked />}
+                label="Extra Chico"
+                sx={{px: 3}}
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Chico"
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Mediano"
+                sx={{px: 3}}
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Grande"
+                sx={{px: 2.7}}
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Extra Grande"
+                sx={{px: 3}}
+              />
+              <Typography sx={{ px: 2, py: 1, fontWeight: "bold" }}>
+                GÉNERO
+              </Typography>
+
+               <RadioGroup
+                aria-labelledby="demo-radio-buttons-group-label"
+                defaultValue="hembra"
+                name="radio-buttons-group"
+                sx={{px: 3}}
+              >
+                <FormControlLabel value="hembra" control={<Radio />} label="Hembra" />
+                <FormControlLabel value="macho" control={<Radio />} label="Macho" />
+
+              </RadioGroup>
+
+              <Typography sx={{ px: 2, py: 1, fontWeight: "bold" }}>
+                EDAD
+              </Typography>
+             
+              <Autocomplete
+                options={ageOptions}
+                getOptionDisabled={(option) => option === ageOptions[4] || option === ageOptions[4]}
+                sx={{ width: 300, px:2, }}
+                renderInput={(params) => <TextField {...params} label="Selecciona una edad" />}
+              />
+            </Menu>
         </Box>
 
         {data.data.length === 0 ? (

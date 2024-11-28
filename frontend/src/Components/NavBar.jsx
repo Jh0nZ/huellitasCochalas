7eaebf447e3c95ce5bdb7421c01b85818b406542
Jh0nZ <43425433+Jh0nZ@users.noreply.github.com>
2024-11-28 10:47:37 -0400

    Add ImageCarousel component for displaying images with navigation; refactor App component to include layout adjustments

--- frontend/src/Components/NavBar.jsx
+++ frontend/src/Components/NavBar.jsx
@@ -1,16 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, forwardRef, useRef } from "react";
 import {
-  AppBar,
-  Container,
-  Toolbar,
-  Typography,
-  Box,
-  IconButton,
-  Button,
-  Tooltip,
-  Avatar,
-  Menu,
-  MenuItem,
+    AppBar,
+    Container,
+    Toolbar,
+    Typography,
+    Box,
+    IconButton,
+    Button,
+    Menu,
+    MenuItem,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useNavigate } from "react-router-dom";
@@ -19,195 +17,201 @@ import { useLogoutUserMutation } from "../features/api/userApi";
 const pages = ["HOME", "REGISTRATE", "PET REGISTER"];
 const settings = ["Perfil", "Cerrar sesión"];
 
-function NavBar() {
-  const navigate = useNavigate();
-  const [user, setUser] = useState(null);
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
-  const [logoutUser, { data, isSuccess, error, isError }] =
-    useLogoutUserMutation();
+const NavBar = () => {
+    const navigate = useNavigate();
+    const [user, setUser] = useState(null);
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+    const [logoutUser, { data, isSuccess, error, isError }] =
+        useLogoutUserMutation();
 
-  useEffect(() => {
-    if (isSuccess) {
-      console.log("Logout successful", data);
-      localStorage.removeItem("user");
-      navigate("/login");
-    }
-    if (isError) {
-      console.log("Logout failed", error);
-    }
-  }, [isSuccess, isError, data, error]);
+    useEffect(() => {
+        if (isSuccess) {
+            console.log("Logout successful", data);
+            localStorage.removeItem("user");
+            navigate("/login");
+        }
+        if (isError) {
+            console.log("Logout failed", error);
+        }
+    }, [isSuccess, isError, data, error]);
 
-  useEffect(() => {
-    const storedUser = localStorage.getItem("user");
-    if (storedUser) {
-      setUser(JSON.parse(storedUser));
-    }
-  }, []);
+    useEffect(() => {
+        const storedUser = localStorage.getItem("user");
+        if (storedUser) {
+            setUser(JSON.parse(storedUser));
+        }
+    }, []);
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
 
-  const handlePageClick = (page) => {
-    handleCloseNavMenu();
-    if (page === "REGISTRATE") {
-      navigate("/register");
-    }
-    if (page === "HOME") {
-      navigate("/home");
-    }
-    if (page === "PET REGISTER") {
-      navigate("/pet-register");
-    }
-  };
+    const handlePageClick = (page) => {
+        handleCloseNavMenu();
+        if (page === "REGISTRATE") {
+            navigate("/register");
+        }
+        if (page === "HOME") {
+            navigate("/home");
+        }
+        if (page === "PET REGISTER") {
+            navigate("/pet-register");
+        }
+    };
 
-  const handleSettingsClick = (settings) => {
-    handleCloseNavMenu();
-    if (settings === "Cerrar sesión") {
-      navigate("/login");
-      logoutUser();
-    }
-    if (settings === "Perfil") {
-      navigate("/perfil");
-    }
-  };
-  return (
-    <AppBar position="fixed" sx={{ backgroundColor: "#9567AB" }}>
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          {/* Logo grande en pantallas md+ */}
-          <Box sx={{ display: { xs: "none", md: "flex" }, mr: 2 }}>
-            <img
-              src="/Huellitas.png"
-              alt="Huellitas icon"
-              style={{ maxWidth: "20%", height: "auto" }}
-            />
-          </Box>
+    const handleSettingsClick = (settings) => {
+        handleCloseNavMenu();
+        if (settings === "Cerrar sesión") {
+            navigate("/login");
+            logoutUser();
+        }
+        if (settings === "Perfil") {
+            navigate("/perfil");
+        }
+    };
+    return (
+        <AppBar sx={{ backgroundColor: "#9567AB"}}>
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    {/* Logo grande para pantallas medianas y grandes */}
+                    <Box sx={{ display: { xs: "none", md: "flex" }, mr: 2 }}>
+                        <img
+                            src="/Huellitas.png"
+                            alt="Huellitas icon"
+                            style={{ width: "120px", height: "auto" }} // Ajuste de tamaño
+                        />
+                    </Box>
 
-          {/* Menú responsivo (Icono de menú en pantallas xs-md) */}
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="menu"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{ display: { xs: "block", md: "none" } }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={() => handlePageClick(page)}>
-                  <Typography sx={{ textAlign: "center" }}>{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+                    {/* Menú hamburguesa para pantallas pequeñas */}
+                    <Box
+                        sx={{
+                            flexGrow: 1,
+                            display: { xs: "flex", md: "none" },
+                        }}
+                    >
+                        <IconButton
+                            size="large"
+                            aria-label="menu"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: "bottom",
+                                horizontal: "left",
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: "top",
+                                horizontal: "left",
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{ display: { xs: "block", md: "none" } }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem
+                                    key={page}
+                                    onClick={() => handlePageClick(page)}
+                                >
+                                    <Typography textAlign="center">
+                                        {page}
+                                    </Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
 
-          {/* Logo pequeño en pantallas xs */}
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            <img
-              src="/Huellitas.png"
-              alt="huellitas icon"
-              style={{ maxWidth: "40%", height: "auto" }}
-            />
-          </Typography>
+                    {/* Logo pequeño para pantallas pequeñas */}
+                    <Box
+                        sx={{
+                            flexGrow: 1,
+                            display: { xs: "flex", md: "none" },
+                            justifyContent: "center",
+                        }}
+                    >
+                        <img
+                            src="/Huellitas.png"
+                            alt="Huellitas icon"
+                            style={{ width: "80px", height: "auto" }} // Tamaño reducido
+                        />
+                    </Box>
 
-          {/* Menú de páginas en pantallas md+ */}
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={() => handlePageClick(page)}
-                sx={{ my: 2, color: "white", display: "block" }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
+                    {/* Botones de navegación para pantallas grandes */}
+                    <Box
+                        sx={{
+                            flexGrow: 1,
+                            display: { xs: "none", md: "flex" },
+                            justifyContent: "center",
+                        }}
+                    >
+                        {pages.map((page) => (
+                            <Button
+                                key={page}
+                                onClick={() => handlePageClick(page)}
+                                sx={{ my: 2, color: "white", display: "block" }}
+                            >
+                                {page}
+                            </Button>
+                        ))}
+                    </Box>
 
-          {/* Menú de usuario */}
-          <Box sx={{ flexGrow: 0 }}>
-            <Button
-              onClick={handleOpenUserMenu}
-              sx={{ my: 2, color: "white", display: "block" }}
-            >
-              Perfil
-            </Button>
-
-            <Menu
-              sx={{ mt: "40px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem
-                  key={setting}
-                  onClick={() => handleSettingsClick(setting)}
-                >
-                  <Typography sx={{ textAlign: "center" }}>
-                    {setting}
-                  </Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
-  );
-}
+                    {/* Menú de usuario */}
+                    <Box sx={{ flexGrow: 0 }}>
+                        <Button
+                            onClick={handleOpenUserMenu}
+                            sx={{ my: 2, color: "white" }}
+                        >
+                            Perfil
+                        </Button>
+                        <Menu
+                            sx={{ mt: "45px" }}
+                            id="menu-appbar"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: "top",
+                                horizontal: "right",
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: "top",
+                                horizontal: "right",
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            {settings.map((setting) => (
+                                <MenuItem
+                                    key={setting}
+                                    onClick={() => handleSettingsClick(setting)}
+                                >
+                                    <Typography textAlign="center">
+                                        {setting}
+                                    </Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+};
 
 export default NavBar;
--- frontend/src/pages/Infopets.jsx
+++ frontend/src/pages/Infopets.jsx
@@ -1,19 +1,10 @@
-import React, { useEffect, useState } from "react";
-import {
-    Grid2 as Grid,
-    Card,
-    CardMedia,
-    CardContent,
-    Typography,
-    Button,
-    useTheme,
-    Box,
-} from "@mui/material";
-import PetsIcon from "@mui/icons-material/Pets";
+import React, { useEffect } from "react";
+import { Typography, Button, useTheme, Box } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import { useGetPetByIdQuery } from "../features/api/petApi";
 import OpenMapLabel from "../components/OpenMapLabel";
+import ImageCarousel from "../components/ImageCarousel";
 
 const Infopets = () => {
     const { pet_id } = useParams();
@@ -56,7 +47,7 @@ const Infopets = () => {
                 alignItems="center"
                 height="100vh"
             >
-                <Typography>{error.data.message}</Typography>
+                <Typography>{error.data?.message}</Typography>
             </Box>
         );
     }
@@ -64,135 +55,71 @@ const Infopets = () => {
     return (
         <Box
             sx={{
-                mt: { xs: 4, md: 7 },
-                px: { xs: 2, sm: 4 },
                 display: "flex",
-                flexDirection: { xs: "column", md: "row" },
-                gap: 4,
-                boxShadow: 3,
-                borderRadius: "10px",
-                overflow: "hidden", // Evita que el contenido se desborde
+                flexDirection: "column",
+                alignItems: "center",
+                width: { xs: "100%", md: "40%" },
+                mx: "auto",
+                padding: 3,
+                gap: 2,
             }}
         >
-            {/* Contenedor completo con la imagen, información y mapa */}
-            <Card
+            {/* Imagen de la mascota */}
+            <ImageCarousel images={data.data.images} />
+
+            {/* Información de la mascota */}
+            <Typography variant="h4" component="h1" gutterBottom>
+                {data.data.name.toUpperCase()}
+            </Typography>
+            <Typography variant="body1">{data.data.description}</Typography>
+
+            <Box sx={{ display: "flex", flexDirection: "row", gap: 2 }}>
+                <Typography variant="body1">EDAD: {data.data.age}</Typography>
+
+                <Typography variant="body1">
+                    TIPO: {data.data.breed.name}
+                </Typography>
+
+                <Typography variant="body1">
+                    TAMAÑO: {data.data.size.name}
+                </Typography>
+            </Box>
+            {/* Mapa */}
+            <Box
                 sx={{
-                    display: "flex",
-                    flexDirection: { xs: "column", md: "row" },
                     width: "100%",
+                    height: { xs: "250px", md: "400px" },
+                    boxShadow: 3,
+                    borderRadius: "10px",
+                    overflow: "hidden",
+                    marginTop: { xs: 3, md: 4 },
                 }}
             >
-                {/* Imagen de la mascota */}
-                <Box
-                    sx={{
-                        width: { xs: "100%", md: "40%" },
-                        height: { xs: "250px", md: "350px" },
+                <OpenMapLabel
+                    location={{
+                        lat: data.data.lat,
+                        lng: data.data.lng,
                     }}
-                >
-                    <CardMedia
-                        component="img"
-                        alt={data.data.name}
-                        image={`http://localhost:8000/storage/${data.data.images[0].path}`}
-                        sx={{
-                            width: "100%",
-                            height: "100%",
-                            objectFit: "cover",
-                            borderRadius: "10px",
-                        }}
-                    />
-                </Box>
+                />
+            </Box>
 
-                {/* Información de la mascota */}
-                <Box
-                    sx={{
-                        width: { xs: "100%", md: "60%" },
-                        padding: { xs: 2, sm: 4 },
-                        display: "flex",
-                        flexDirection: "column",
-                    }}
-                >
-                    <CardContent sx={{ flex: 1 }}>
-                        <Typography variant="h4" component="h1" gutterBottom>
-                            {data.data.name.toUpperCase()}
-                        </Typography>
-                        <Typography variant="body1" paragraph>
-                            {data.data.description}
-                        </Typography>
-
-                        <Typography
-                            variant="body1"
-                            sx={{
-                                display: "flex",
-                                alignItems: "center",
-                                mb: 1,
-                            }}
-                        >
-                            <PetsIcon sx={{ mr: 1 }} /> EDAD: {data.data.age}
-                        </Typography>
-
-                        <Typography
-                            variant="body1"
-                            sx={{
-                                display: "flex",
-                                alignItems: "center",
-                                mb: 1,
-                            }}
-                        >
-                            <PetsIcon sx={{ mr: 1 }} /> RAZA:{" "}
-                            {data.data.breed.name}
-                        </Typography>
-
-                        <Typography
-                            variant="body1"
-                            sx={{
-                                display: "flex",
-                                alignItems: "center",
-                                mb: 1,
-                            }}
-                        >
-                            <PetsIcon sx={{ mr: 1 }} /> TAMAÑO:{" "}
-                            {data.data.size.name}
-                        </Typography>
-
-                        {/* Botón de Adopción */}
-                        <Button
-                            variant="contained"
-                            size="large"
-                            fullWidth
-                            sx={{
-                                backgroundColor: theme.palette.primary.main,
-                                "&:hover": {
-                                    backgroundColor: theme.palette.primary.dark,
-                                },
-                                mt: 4,
-                                py: 1.5,
-                            }}
-                            onClick={handleAdoptaClick}
-                        >
-                            ADOPTAR
-                        </Button>
-                    </CardContent>
-
-                    {/* Mapa */}
-                    <Box
-                        sx={{
-                            width: "100%",
-                            height: { xs: "250px", md: "400px" },
-                            boxShadow: 3,
-                            borderRadius: "10px",
-                            overflow: "hidden",
-                            marginTop: { xs: 3, md: 4 },
-                        }}
-                    >
-                        <OpenMapLabel
-                            location={{
-                                lat: data.data.lat,
-                                lng: data.data.lng,
-                            }}
-                        />
-                    </Box>
-                </Box>
-            </Card>
+            {/* Botón de Adopción */}
+            <Button
+                variant="contained"
+                size="large"
+                fullWidth
+                sx={{
+                    backgroundColor: theme.palette.primary.main,
+                    "&:hover": {
+                        backgroundColor: theme.palette.primary.dark,
+                    },
+                    mt: 4,
+                    py: 1.5,
+                }}
+                onClick={handleAdoptaClick}
+            >
+                ADOPTAR
+            </Button>
         </Box>
     );
 };
